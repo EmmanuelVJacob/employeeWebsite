4373dc949f7cb85819bd5d7b3b7e76a5446a8e81
Emmanuel Jacob <emmanueljacob678@gmail.com>
2024-08-31 23:13:18 +0530

    debounce

--- frontend/src/Components/SearchInput.jsx
+++ frontend/src/Components/SearchInput.jsx
@@ -1,14 +1,35 @@
-
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const SearchInput = ({ search, setSearch }) => {
+  const [inputValue, setInputValue] = useState(search);
+
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const handleInputChange = useCallback(
+    debounce((value) => setSearch(value), 500),
+    []
+  );
+
+  const onChange = (e) => {
+    setInputValue(e.target.value);
+    handleInputChange(e.target.value);
+  };
+
   return (
     <div className="mb-4 flex items-center mt-6">
       <input
         type="text"
         placeholder="Search..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        value={inputValue}
+        onChange={onChange}
         className="p-2 border border-gray-300 bg-custompink rounded"
       />
     </div>
